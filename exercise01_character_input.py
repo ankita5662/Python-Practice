466d6b543bed9eb56a00e8458b975fed543be46b
ankita5662 <ankitasingh5662@gmail.com>
2026-08-31 17:17:55 +0530

    Updated exercise 1: added multi-line output

--- exercise01_character_input.py
+++ exercise01_character_input.py
@@ -7,3 +7,25 @@ name = str(input("enter your name: "))
 age = int(input("enter your current age: "))
 year = 2026 - age + 100
 print ( name + " , will turn 100 years old in  " + str(year) )
+
+
+'''Extras:
+1. Add on to the previous program by asking the user for another number and 
+printing out that many copies of the previous message
+2. Print out that many copies of the previous message on separate lines. 
+(Hint: the string "\n is the same as pressing the ENTER button)'''
+
+# 1. 
+name = str(input("enter your name: "))
+age = int(input("enter your current age: "))
+num = int(input("enter a number: "))
+year = 2026 - age + 100
+print (num*(name + " , will turn 100 years old in  " + str(year)))
+
+# 2. 
+name = str(input("enter your name: "))
+age = int(input("enter your current age: "))
+num = int(input("enter a number: "))
+year = 2026 - age + 100
+message = name + " , will turn 100 years old in  " + str(year) + "\n"
+print(message*num)
