d8c129beebda6138df268b738a5ff9fdac181656
ankita5662 <ankitasingh5662@gmail.com>
2026-09-11 16:53:31 +0530

    Solved exercise14: List Remove duplicates

---
+++ exercise14_List_Remove_Duplicates.py
@@ -0,0 +1,19 @@
+'''Write a program (function!) that takes a list and returns a new list that contains 
+all the elements of the first list minus all the duplicates. 
+Extras:
+1. Write two different functions to do this - one using a loop and constructing a list, and another using sets.
+2. Go back and do Exercise 5 using sets, and write the solution for that in a different function.'''
+
+import random
+
+def list1(n):
+    b = []
+    for i in n:
+        if i not in b:
+            b.append(i)
+    return b
+
+a = random.choices( range(1 , 10) , k=12)
+print (a)
+print ( list1 (a) )
+
