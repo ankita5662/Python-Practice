c3823c01c5ee172aff0673921c268a64484c4f7a
ankita5662 <ankitasingh5662@gmail.com>
2026-09-11 17:20:00 +0530

    Complete exercise 14: list remove duplicates with loop and set versions

--- exercise14_List_Remove_Duplicates.py
+++ exercise14_List_Remove_Duplicates.py
@@ -1,7 +1,8 @@
 '''Write a program (function!) that takes a list and returns a new list that contains 
 all the elements of the first list minus all the duplicates. 
 Extras:
-1. Write two different functions to do this - one using a loop and constructing a list, and another using sets.
+1. Write two different functions to do this - one using a loop and constructing a list, 
+and another using sets.
 2. Go back and do Exercise 5 using sets, and write the solution for that in a different function.'''
 
 import random
@@ -17,3 +18,16 @@ a = random.choices( range(1 , 10) , k=12)
 print (a)
 print ( list1 (a) )
 
+# Extras:
+# 1. 
+def list2(n):
+    return list(set(n))
+print("Set version:", list2(a))
+
+# 2. 
+def list_overlap_set(a, b):
+    return list(set(a) & set(b))
+
+x = [1, 2, 3, 4, 5]
+y = [3, 4, 5, 6, 7]
+print("Overlap (set):", list_overlap_set(x, y))
