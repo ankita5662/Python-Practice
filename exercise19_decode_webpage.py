66f70620610bc1448547d384c37bbe50a6507e19
ankita5662 <ankitasingh5662@gmail.com>
2026-09-16 22:21:47 +0530

    Solved exercise19: decode webpage 2

---
+++ exercise19_decode_webpage.py
@@ -0,0 +1,18 @@
+'''Using the requests and BeautifulSoup Python libraries, print to the screen
+ the full text of the article on this website: 
+ http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture. 
+The article is long, so it is split up between 4 pages. Your task is to print out the 
+text to the screen so that you can read the full article without having to click any buttons.'''
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+paragraphs = soup.find_all("p")
+
+for p in paragraphs:
+    print(p.get_text())
